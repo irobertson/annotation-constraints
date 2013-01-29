61556ec0b7b7c919b18b3460bbdcecf931bab113
hansenc <hansen.chris.w@gmail.com>
2013-01-29 14:14:09 -0700

    JavaDoc for VerifierUtils.

--- src/main/java/com/overstock/constraint/verifier/VerifierUtils.java
+++ src/main/java/com/overstock/constraint/verifier/VerifierUtils.java
@@ -8,15 +8,18 @@ import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.VariableElement;
 
 public class VerifierUtils {
 
-  public static List<AnnotationValue> getArrayValues(AnnotationMirror annotation) {
-    return getArrayValues(annotation, "value");
-  }
-
+  /**
+   * Gets the array values for the element on the annotation with the specified name.
+   *
+   * @param annotation the annotation
+   * @param  elementName the name of the element on the annotation
+   * @return the array values for the element on the annotation with the specified name
+   */
   public static List<AnnotationValue> getArrayValues(AnnotationMirror annotation, String elementName) {
     Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = annotation.getElementValues();
     for (ExecutableElement executableElement : elementValues.keySet()) {
@@ -29,6 +32,23 @@ public class VerifierUtils {
     throw new IllegalStateException(annotation + "." + elementName + " was not found");
   }
 
+  /**
+   * Gets the array values for the element named "{@code value}" on the annotation.
+   *
+   * @param annotation the annotation
+   * @return the array values for the element named "{@code value}" on the annotation
+   */
+  public static List<AnnotationValue> getArrayValues(AnnotationMirror annotation) {
+    return getArrayValues(annotation, "value");
+  }
+
+  /**
+   * Gets the class names represented by the annotation values.
+   *
+   * @param annotationValues the annotation values
+   * @return the class names represented by the annotation values
+   * @see #getClassName(javax.lang.model.element.AnnotationValue)
+   */
   public static Set<String> getClassNames(Collection<AnnotationValue> annotationValues) {
     Set<String> classNames = new HashSet<String>(annotationValues.size());
     for (AnnotationValue annotationValue : annotationValues) {
@@ -37,17 +57,35 @@ public class VerifierUtils {
     return classNames;
   }
 
+  /**
+   * Gets the class name represented by the annotation value.
+   *
+   * @param annotationValue the annotation value
+   * @return the class name represented by the annotation value
+   */
   public static String getClassName(AnnotationValue annotationValue) {
     return annotationValue.getValue().toString(); //TODO does this work for other compilers besides javac?
   }
 
-  public static String getClassName(VariableElement variableElement) {
-    return variableElement.asType().toString();
+  /**
+   * Gets the class name of the element's type.
+   *
+   * @param element the element
+   * @return the class name of the element's type
+   */
+  public static String getClassName(Element element) {
+    return element.asType().toString();
+  }
+
+  /**
+   * Gets the class name of the annotation's type.
+   *
+   * @param annotation the annotation
+   * @return the class name of the annotation's type
+   */
+  public static String getClassName(AnnotationMirror annotation) {
+    return annotation.getAnnotationType().asElement().toString();
   }
 
   private VerifierUtils() {}
-
-  public static String getClassName(AnnotationMirror annotated) {
-    return annotated.getAnnotationType().asElement().toString();
-  }
 }
