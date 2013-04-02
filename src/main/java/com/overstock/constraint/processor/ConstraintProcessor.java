1adff3198ae649d83348ce452513512e2cf5d39f
hansenc <hansen.chris.w@gmail.com>
2013-04-02 10:31:35 -0600

    Move verifier creation out of ConstraintProcessor.

--- src/main/java/com/overstock/constraint/processor/ConstraintProcessor.java
+++ src/main/java/com/overstock/constraint/processor/ConstraintProcessor.java
@@ -1,15 +1,15 @@
 package com.overstock.constraint.processor;
 
+import static com.overstock.constraint.processor.VerifierFactory.VerifierInstantiationException;
+import static com.overstock.constraint.processor.VerifierFactory.VerifierNotFoundException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -18,26 +18,18 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
-import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 
-import com.overstock.constraint.Constraint;
 import com.overstock.constraint.provider.ProvidesConstraintsFor;
 import com.overstock.constraint.verifier.Verifier;
 
 @SupportedAnnotationTypes("*")
 public class ConstraintProcessor extends AbstractProcessor {
-
-  private Map<DeclaredType, Verifier> verifiers = new HashMap<DeclaredType, Verifier>();
-
   private ProvidedConstraints providedConstraints;
 
   private ClassLoader classLoader;
@@ -61,6 +53,7 @@ public class ConstraintProcessor extends AbstractProcessor {
     ProvidedConstraints roundConstraints = ProvidedConstraints.from(
       roundEnv.getElementsAnnotatedWith(ProvidesConstraintsFor.class), processingEnv);
     providedConstraints = providedConstraints.combineWith(roundConstraints);
+    VerifierFactory verifierFactory = new VerifierFactory(processingEnv, classLoader);
 
     try {
       Elements elementUtils = processingEnv.getElementUtils();
@@ -69,24 +62,21 @@ public class ConstraintProcessor extends AbstractProcessor {
           Constraints constraints = Constraints.on(constrained, providedConstraints, processingEnv);
           for (ConstraintMirror constraint : constraints) {
             DeclaredType constraintType = constraint.getAnnotation().getAnnotationType();
-            Verifier verifier = verifiers.get(constraintType);
-            if (verifier == null) {
-              try {
-                verifier = initializeVerifier(constraint);
-                verifiers.put(constraintType, verifier);
-              }
-              catch (VerifierNotFoundException e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                  "The verifier for " + constraintType + " was not found, which is required for "
-                    + constrained.getAnnotationType(), element, constrained);
-                continue;
-              }
-              catch (VerifierInstantiationException e) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Error instantiating verifier "
-                  + e.verifierType + " which is required for " + constrained.getAnnotationType()
-                  + " due to an exception: " + e.getMessage(), element, constrained);
-                continue;
-              }
+            final Verifier verifier;
+            try {
+              verifier = verifierFactory.createVerifier(constraint);
+            }
+            catch (VerifierNotFoundException e) {
+              processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                "The verifier for " + constraintType + " was not found, which is required for "
+                  + constrained.getAnnotationType(), element, constrained);
+              continue;
+            }
+            catch (VerifierInstantiationException e) {
+              processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Error instantiating verifier "
+                + e.getVerifierType() + " which is required for " + constrained.getAnnotationType()
+                + " due to an exception: " + e.getMessage(), element, constrained);
+              continue;
             }
             try {
               verifier.verify(element, constrained, constraint);
@@ -103,39 +93,6 @@ public class ConstraintProcessor extends AbstractProcessor {
       MirrorUtils.clearCaches(); //in Eclipse we don't want caches hanging around
     }
     return false;
-  }
-
-  private Verifier initializeVerifier(ConstraintMirror constraint) throws VerifierNotFoundException,
-      VerifierInstantiationException {
-    TypeMirror constraintMirror = MirrorUtils.getTypeMirror(Constraint.class, processingEnv.getElementUtils());
-    Types typeUtils = processingEnv.getTypeUtils();
-    List<? extends AnnotationMirror> annotationMirrors = constraint.getAnnotation().getAnnotationType().asElement()
-      .getAnnotationMirrors();
-    for (AnnotationMirror annotationMirror : annotationMirrors) {
-      if (typeUtils.isSameType(constraintMirror, annotationMirror.getAnnotationType())) {
-        Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = annotationMirror.getElementValues();
-        for (ExecutableElement executableElement : elementValues.keySet()) {
-          if (executableElement.getSimpleName().contentEquals("verifiedBy")) {
-            TypeMirror verifierType = (TypeMirror) elementValues.get(executableElement).getValue();
-            Verifier verifier;
-            try {
-              verifier = newInstance(verifierType);
-            }
-            catch (Exception e) {
-              throw new VerifierInstantiationException(verifierType, e);
-            }
-            verifier.init(processingEnv);
-            return verifier;
-          }
-        }
-
-      }
-    }
-    throw new VerifierNotFoundException();
-  }
-
-  private Verifier newInstance(TypeMirror verifierType) throws Exception {
-    return (Verifier) Class.forName(verifierType.toString(), true, classLoader).newInstance();
   }
 
   private Set<? extends Element> elementsToProcess(RoundEnvironment roundEnv) {
@@ -186,18 +143,6 @@ public class ConstraintProcessor extends AbstractProcessor {
       throw new RuntimeException("Could not read constraint providers file", e);
     }
     return constraintProviders;
-  }
-
-  private static class VerifierNotFoundException extends Exception {}
-
-  private static class VerifierInstantiationException extends Exception {
-
-    private final TypeMirror verifierType;
-
-    public VerifierInstantiationException(TypeMirror verifierType, Throwable cause) {
-      super(cause);
-      this.verifierType = verifierType;
-    }
   }
 
 }
---
+++ src/main/java/com/overstock/constraint/processor/VerifierFactory.java
@@ -0,0 +1,85 @@
+package com.overstock.constraint.processor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Types;
+
+import com.overstock.constraint.Constraint;
+import com.overstock.constraint.verifier.Verifier;
+
+class VerifierFactory {
+  private final Map<DeclaredType, Verifier> verifiers = new HashMap<DeclaredType, Verifier>();
+  private final ProcessingEnvironment processingEnv;
+  private final ClassLoader classLoader;
+
+  public VerifierFactory(ProcessingEnvironment processingEnv, ClassLoader classLoader) {
+    this.processingEnv = processingEnv;
+    this.classLoader = classLoader;
+  }
+
+  public Verifier createVerifier(ConstraintMirror constraint)
+    throws VerifierInstantiationException, VerifierNotFoundException {
+    DeclaredType constraintType = constraint.getAnnotation().getAnnotationType();
+    Verifier verifier = verifiers.get(constraintType);
+    if (verifier != null) {
+      return verifier; //used cached value
+    }
+
+    TypeMirror constraintMirror = MirrorUtils.getTypeMirror(Constraint.class, processingEnv.getElementUtils());
+    Types typeUtils = processingEnv.getTypeUtils();
+    List<? extends AnnotationMirror> annotationMirrors = constraint.getAnnotation().getAnnotationType().asElement()
+      .getAnnotationMirrors();
+    for (AnnotationMirror annotationMirror : annotationMirrors) {
+      if (typeUtils.isSameType(constraintMirror, annotationMirror.getAnnotationType())) {
+        Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = annotationMirror.getElementValues();
+        for (ExecutableElement executableElement : elementValues.keySet()) {
+          if (executableElement.getSimpleName().contentEquals("verifiedBy")) {
+            verifier = createVerifier((TypeMirror) elementValues.get(executableElement).getValue());
+            verifiers.put(constraintType, verifier); //cache
+            return verifier;
+          }
+        }
+      }
+    }
+    throw new VerifierNotFoundException();
+  }
+
+  private Verifier createVerifier(TypeMirror verifierType) throws VerifierInstantiationException {
+    Verifier verifier;
+    try {
+      verifier = newInstance(verifierType);
+    }
+    catch (Exception e) {
+      throw new VerifierInstantiationException(verifierType, e);
+    }
+    verifier.init(processingEnv);
+    return verifier;
+  }
+
+  private Verifier newInstance(TypeMirror verifierType) throws Exception {
+    return (Verifier) Class.forName(verifierType.toString(), true, classLoader).newInstance();
+  }
+
+  public static class VerifierNotFoundException extends Exception {}
+
+  public static class VerifierInstantiationException extends Exception {
+    private final TypeMirror verifierType;
+
+    public VerifierInstantiationException(TypeMirror verifierType, Throwable cause) {
+      super(cause);
+      this.verifierType = verifierType;
+    }
+
+    public TypeMirror getVerifierType() {
+      return verifierType;
+    }
+  }
+}
